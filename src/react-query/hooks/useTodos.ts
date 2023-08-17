5ee0c7922c25782e8c03fa3e4e24a1a022f91b5b
Sugeng Wahyudi <sugengdatascience@gmail.com>
2023-08-18 06:21:58 +0700

    done http service

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,20 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
+import TodoServices, { Todo } from "../services/TodoServices";
 
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
-
-const apiClient = new APIClient<Todo>("/todos");
 const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: [CACHE_KEY_TODOS],
-    queryFn: apiClient.getAll,
+    queryFn: TodoServices.getAll,
     staleTime: 10 * 1000,
   });
 };
